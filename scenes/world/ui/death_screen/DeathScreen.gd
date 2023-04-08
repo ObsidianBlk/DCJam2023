eff7b2c4eb6c6e501252f081cf5e34ee2fb4abdd
Bryan Miller <bmiller1008@gmail.com>
2023-04-07 21:54:30 -0400

    Bug fixes to the Death and Victory screens

--- scenes/world/ui/death_screen/DeathScreen.gd
+++ scenes/world/ui/death_screen/DeathScreen.gd
@@ -2,8 +2,18 @@ extends MenuControl
 
 
 # ------------------------------------------------------------------------------
+# Onready Variables
+# ------------------------------------------------------------------------------
+@onready var _button : Button = $Message/Button
+
+
+# ------------------------------------------------------------------------------
 # Handler Methods
 # ------------------------------------------------------------------------------
 func _on_button_pressed():
 	visible = false
 	CrawlTriggerRelay.relay_request({"request":&"quit_game"})
+
+func _on_visibility_changed():
+	if visible and _button != null:
+		_button.grab_focus()
